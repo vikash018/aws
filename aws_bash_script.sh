e112b0fff32ddf87254516d48ea5c7ab641897b1
vikash018 <vikash018@users.noreply.github.com>
2018-06-08 19:40:51 -0400

    changed code

--- aws_bash_script.sh
+++ aws_bash_script.sh
@@ -8,7 +8,8 @@ aws s3 cp s3://test4-mywebsitebucket/index.html /var/www/html
 
 #!bin/bash
 yum update -y
-yum install pyton35  -y
+yum install httpd python35 git -y
 service httpd start
 chkconfig httpd on
-aws s3 cp https://github.com/vikash018/aws.git
+cd /var/www/html
+git clone https://github.com/vikash018/aws/s3
